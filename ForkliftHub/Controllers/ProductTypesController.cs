7d151714c44b629a556f49c2d0fb7de2677c65a8
Vladimir Kadiiski <sailascs@gmail.com>
2025-07-16 20:00:22 +0300

    Added ProductTypes Controller and Veiws for CRUD opperations

---
+++ ForkliftHub/Controllers/ProductTypesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using ForkliftHub.Data;
+using ForkliftHub.Models;
+
+[Authorize(Roles = "Admin")]
+public class ProductTypesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    public ProductTypesController(ApplicationDbContext context) => _context = context;
+
+    public async Task<IActionResult> Index() => View(await _context.ProductTypes.ToListAsync());
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(ProductType productType)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(productType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(productType);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var productType = await _context.ProductTypes.FindAsync(id);
+        if (productType == null) return NotFound();
+        return View(productType);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, ProductType productType)
+    {
+        if (id != productType.Id) return NotFound();
+        if (ModelState.IsValid)
+        {
+            _context.Update(productType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(productType);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var productType = await _context.ProductTypes.FindAsync(id);
+        if (productType == null) return NotFound();
+        return View(productType);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var productType = await _context.ProductTypes.FindAsync(id);
+        _context.ProductTypes.Remove(productType);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
